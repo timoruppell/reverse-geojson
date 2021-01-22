586dd7fca95489dba553a8fd22fe125ab282a554
Timo Rüppell <timo.ruppell@gmail.com>
2021-01-22 23:40:18 +0200

    Add more countires

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const borderJson = require('./antarctica-border.json');
 // const borderJson = require('./china-border.json');
 // const borderJson = require('./finland-border.json');
-const borderJson = require('./countries.json');
+// const borderJson = require('./countries.json');
 
 function reverse (c) {
     c[0] = Number(Number(c[0] - 180).toFixed(4));
@@ -18,6 +19,11 @@ borderJson.features.forEach(f => {
     }
 });
 
+fs.writeFile('antarctica-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
+  if (err) return console.log(err);
+  console.log('Wrote antarctica-reversed.json');
+});
+
 // fs.writeFile('china-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
 //   if (err) return console.log(err);
 //   console.log('Wrote china-reversed.json');
@@ -28,7 +34,7 @@ borderJson.features.forEach(f => {
 //   console.log('Wrote finland-reversed.json');
 // });
 
-fs.writeFile('countires-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
-  if (err) return console.log(err);
-  console.log('Wrote countires-reversed.json');
-});
+// fs.writeFile('countires-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
+//   if (err) return console.log(err);
+//   console.log('Wrote countires-reversed.json');
+// });
