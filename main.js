5fc07e81381300b412eaacadb6b1c792f0b3166d
Timo Rüppell <timo.ruppell@gmail.com>
2021-01-22 23:11:35 +0200

    Add more countires

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-const borderJson = require('./china-border.geojson');
+// const borderJson = require('./china-border.json');
+// const borderJson = require('./finland-border.json');
+const borderJson = require('./countries.json');
 
 function reverse (c) {
     c[0] = Number(Number(c[0] - 180).toFixed(4));
@@ -16,7 +18,17 @@ borderJson.features.forEach(f => {
     }
 });
 
-fs.writeFile('china-reversed.geojson', JSON.stringify(borderJson, null, 4), function (err) {
+// fs.writeFile('china-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
+//   if (err) return console.log(err);
+//   console.log('Wrote china-reversed.json');
+// });
+
+// fs.writeFile('finland-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
+//   if (err) return console.log(err);
+//   console.log('Wrote finland-reversed.json');
+// });
+
+fs.writeFile('countires-reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
   if (err) return console.log(err);
-  console.log('Wrote china-reversed.geojson');
+  console.log('Wrote countires-reversed.json');
 });
