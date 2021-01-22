05d7bae36c6b47c860c8b116ce292bd1c9eef149
Timo Rüppell <timo.ruppell@gmail.com>
2021-01-22 22:48:26 +0200

    Update reversed

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
 const borderJson = require('./border.json');
 
+function reverse (c) {
+    c[0] = Number(Number(c[0] - 180).toFixed(4));
+    c[1] = -c[1];
+}
+
 borderJson.features.forEach(f => {
     f.properties = {};
-    f.geometry.coordinates[0].forEach(c => {
-        c[0] = Number(Number(c[0] - 180).toFixed(4));
-        c[1] = -c[1];
-    });
+    if (f.type === "Polygon") {
+        f.geometry.coordinates[0].forEach(reverse);
+    }
+    if (f.type === "MultiPolygon") {
+        f.geometry.coordinates.forEach(a => a[0].forEach(reversed));
+    }
 });
 
 fs.writeFile('reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
