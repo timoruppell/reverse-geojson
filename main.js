54f54c87d50e6e837a6f89d51750154e8d96cc0e
Timo Rüppell <timo.ruppell@gmail.com>
2021-01-22 22:30:39 +0200

    Update reversed

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const borderJson = require('./border.json');
 
 borderJson.features.forEach(f => {
     f.geometry.coordinates[0].forEach(c => {
-        c[0] -= 180;
+        c[0] = Number(Number(c[0] - 180).toFixed(4));
         c[1] = -c[1];
     });
 });
 
-fs.writeFile('reversed.json', JSON.stringify(borderJson), function (err) {
+fs.writeFile('reversed.json', JSON.stringify(borderJson, null, 4), function (err) {
   if (err) return console.log(err);
   console.log('Wrote reversed.json');
 });
